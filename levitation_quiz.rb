edfda045ee135c1d543f6033c3680179108e1c1d
Jordan Harris <jharris@wework.com>
2018-07-16 21:25:13 +0000

    automatically backed up by learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,6 @@
 
-def levitation_quiz(input)
-  puts 'What is the spell that enacts levitation?' until input == "Wingardium Leviosa"
+def levitation_quiz
+  puts 'What is the spell that enacts levitation?' 
 end
 
 
